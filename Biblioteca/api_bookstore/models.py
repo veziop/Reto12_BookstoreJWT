6d6fb10bfe261d782acbe5e70f9a8f5a62cbe113
Valentin <valenp97@gmail.com>
2022-01-26 17:23:56 +0100

    Implementado los JWT; JSON Web Token.

--- Biblioteca/api_bookstore/models.py
+++ Biblioteca/api_bookstore/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 from django.contrib.auth.models import User
 
 
 class Author(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    created_date = models.DateField(default=timezone.now)
-    added_by_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateField(auto_now_add=True)
+    added_by_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -18,7 +19,7 @@ class Book(models.Model):
     title = models.CharField(max_length=150)
     description = models.CharField(max_length=255)
     author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
-    created_date = models.DateField(default=timezone.now)
+    created_date = models.DateField(auto_now_add=True)
     added_by_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
